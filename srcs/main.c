c624bd4916a68ee779c9a87f52bb1c3a2947ae64
phnguyen <phnguyen@student.42.fr>
2020-08-21 05:37:03 +0200

    fix args

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: phnguyen <phnguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/08 14:37:59 by phnguyen          #+#    #+#             */
-/*   Updated: 2020/08/05 02:34:43 by phnguyen         ###   ########.fr       */
+/*   Updated: 2020/08/21 05:35:10 by phnguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,15 @@ int	main(int ac, char **av)
 	int		screen;
 
 	screen = 0;
-	g = NULL;
+	if (!(g = (t_game*)malloc(sizeof(t_game))))
+		error_exit("Error: Could'n malloc.\n", g);
+	bzero(g, sizeof(t_game));
 	if (ac == 3 && (ft_strncmp(av[2], "--save", 7) == 0))
 		screen = 1;
 	if (ac == (2 + screen))
-	{
-		if (!(g = (t_game*)malloc(sizeof(t_game))))
-			error_exit("Error: Could'n malloc.\n", g);
-		bzero(g, sizeof(t_game));
 		ft_game(g, screen, av[1]);
-	}
 	else
 		error_exit("Error: Too much/Too few argument.\n", g);
 	exit(0);
+	return (0);
 }
